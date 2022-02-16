928a127e7edcc85c6933c91f5efcee087ab57aec
tainakanchu <tainakanchu815@gmail.com>
2022-02-17 01:32:53 +0900

    edit icecast type

--- src/types/IceCast.d.ts
+++ src/types/IceCast.d.ts
@@ -1,30 +1,33 @@
-export type IcecastStats = typeof icecastStatsExampleData;
-
-const icecastStatsExampleData = {
+export type IcecastStats = {
   icestats: {
-    admin: "icemaster@localhost",
-    host: "localhost",
-    location: "Earth",
-    server_id: "Icecast 2.4.4",
-    server_start: "Fri, 09 Apr 2021 21:49:50 +0200",
-    server_start_iso8601: "2021-04-09T21:49:50+0200",
+    admin: string;
+    host: string;
+    location: string;
+    server_id: string;
+    server_start: string;
+    server_start_iso8601: string;
     source: {
-      audio_bitrate: 128000,
-      audio_channels: 1,
-      audio_samplerate: 48000,
-      bitrate: 128,
-      genre: "various",
-      "ice-bitrate": 128,
-      listener_peak: 1,
-      listeners: 0,
-      listenurl: "http://localhost:8000/test2",
-      server_description: "Unspecified description",
-      server_name: "Unspecified name",
-      server_type: "application/ogg",
-      stream_start: "Fri, 09 Apr 2021 21:49:52 +0200",
-      stream_start_iso8601: "2021-04-09T21:49:52+0200",
-      subtype: "Vorbis",
-      dummy: null,
-    },
-  },
+      artist: string;
+      audio_bitrate: number;
+      audio_channels: number;
+      audio_info: string;
+      audio_samplerate: number;
+      bitrate: string;
+      genre: string;
+      "ice-bitrate": number;
+      "ice-channels": number;
+      "ice-samplerate": number;
+      listener_peak: number;
+      listeners: number;
+      listenurl: string;
+      server_description: string;
+      server_name: string;
+      server_type: string;
+      stream_start: string;
+      stream_start_iso8601: string;
+      subtype: string;
+      title: string;
+      dummy: null;
+    };
+  };
 };
