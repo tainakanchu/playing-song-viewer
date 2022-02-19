0204739ebdc8c8953d92f873efcb62cf745c892d
tainakanchu <tainakanchu815@gmail.com>
2022-02-19 13:06:57 +0900

    refactor type

--- src/types/IceCast.d.ts
+++ src/types/IceCast.d.ts
@@ -1,3 +1,27 @@
+export type SongInfo = {
+  artist: string;
+  audio_bitrate: number;
+  audio_channels: number;
+  audio_info: string;
+  audio_samplerate: number;
+  bitrate: string;
+  genre: string;
+  "ice-bitrate": number;
+  "ice-channels": number;
+  "ice-samplerate": number;
+  listener_peak: number;
+  listeners: number;
+  listenurl: string;
+  server_description: string;
+  server_name: string;
+  server_type: string;
+  stream_start: string;
+  stream_start_iso8601: string;
+  subtype: string;
+  title: string;
+  dummy: null;
+};
+
 export type IcecastStats = {
   icestats: {
     admin: string;
@@ -6,28 +30,6 @@ export type IcecastStats = {
     server_id: string;
     server_start: string;
     server_start_iso8601: string;
-    source: {
-      artist: string;
-      audio_bitrate: number;
-      audio_channels: number;
-      audio_info: string;
-      audio_samplerate: number;
-      bitrate: string;
-      genre: string;
-      "ice-bitrate": number;
-      "ice-channels": number;
-      "ice-samplerate": number;
-      listener_peak: number;
-      listeners: number;
-      listenurl: string;
-      server_description: string;
-      server_name: string;
-      server_type: string;
-      stream_start: string;
-      stream_start_iso8601: string;
-      subtype: string;
-      title: string;
-      dummy: null;
-    };
+    source: SongInfo;
   };
 };
