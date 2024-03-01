bc29ac935850572249bd7e0e69512dd76455cfe7
tainakanchu <tainakanchu815@gmail.com>
2024-03-02 07:03:04 +0900

    APIでサーバー側のファイルを検索して画像データを返すように修正

--- app/api/song-detail/route.ts
+++ app/api/song-detail/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest} from "next/server";
 
 import {glob} from "glob";
+import mm from "music-metadata";
 
 const itunesMediaDirectorySlash =
   "C:\\Users\\kanch\\Music\\iTunes\\iTunes Media\\Music\\";
@@ -15,17 +16,44 @@ export const GET = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
 
   const artist = searchParams.get("artist");
-  const title = searchParams.get("title");
+  const longTitle = searchParams.get("title");
 
-  const pattern = `${itunesMediaDirectoryBackSlash}*${artist}*/**/*${title}*`;
+  if (!longTitle) {
+    // 曲名がないのはさすがに無理
+    return new Response("error", {status: 400});
+  }
+
+  // itunes が整理したファイルはファイル名が最大36文字なので、余裕をもって32文字で切る
+  const title = longTitle.slice(0, 32);
+
+  const patternWithArtist = `${itunesMediaDirectoryBackSlash}*${artist}*/**/*${title}*`;
+  const patternOnlyTitle = `${itunesMediaDirectoryBackSlash}**/*${title}*`;
+
+  console.log("💖pattern", patternWithArtist);
 
   console.log("💖glob");
 
   try {
-    const results = await glob(pattern);
-    console.log("💖results", results);
+    const results = await glob([patternWithArtist, patternOnlyTitle]);
 
-    return new Response(JSON.stringify(results), {});
+    if (results.length === 0) {
+      console.error("💖no result");
+      throw new Error("No result");
+    }
+
+    const metadata = await mm.parseFile(results[0]);
+    const picture = metadata.common.picture
+      ? metadata.common.picture[0].data
+      : null;
+
+    console.log("💖metadata.common", metadata.common);
+
+    const result = {
+      picture,
+      albumArtist: metadata.common.albumartist ?? metadata.common.artist ?? "",
+    };
+
+    return new Response(JSON.stringify(result), {});
   } catch (error) {
     console.error("💖error", error);
 
